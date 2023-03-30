13746b4b1d08287fd00db16935a3a119e7e5bea9
DESKTOP-SIT8ET0\info <d-fujita@a-manage.jp>
2023-03-30 17:34:05 +0900

    PC版データ投入

--- index.php
+++ index.php
@@ -26,15 +26,35 @@
   <meta property="og:image" content="https://www.nonoguchi.net/Image/common/ogp.jpg">
 
   <script>
-  (function(d) {
-    var config = {
-      kitId: 'lsw4czv',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
-</script>
+    (function(d) {
+      var config = {
+          kitId: 'lsw4czv',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h = d.documentElement,
+        t = setTimeout(function() {
+          h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+        }, config.scriptTimeout),
+        tk = d.createElement("script"),
+        f = false,
+        s = d.getElementsByTagName("script")[0],
+        a;
+      h.className += " wf-loading";
+      tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+      tk.async = true;
+      tk.onload = tk.onreadystatechange = function() {
+        a = this.readyState;
+        if (f || a && a != "complete" && a != "loaded") return;
+        f = true;
+        clearTimeout(t);
+        try {
+          Typekit.load(config)
+        } catch (e) {}
+      };
+      s.parentNode.insertBefore(tk, s)
+    })(document);
+  </script>
   <link href="//cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css" rel="stylesheet">
   <link href="https://www.nonoguchi.net//CSS/initialize.min.css" rel="stylesheet" type="text/css">
   <link href="https://www.nonoguchi.net//CSS/common.css" rel="stylesheet" type="text/css">
@@ -136,14 +156,14 @@
 
             <article>
               <section class="section-01">
-              <div class="ttl_wrap">
-                      <h2 class="ttl_01">MESSAGE</h2>
-                      <p class="lead">メッセージ</p>
-                    </div>
+                <div class="ttl_wrap">
+                  <h2 class="ttl_01">MESSAGE</h2>
+                  <p class="lead">メッセージ</p>
+                </div>
                 <div class="container">
-                
+
                   <div class="contetnts">
-                    
+
                     <p class="text">
                       <strong>One for all All for one<br>
                         一人は皆のために、皆は一人のために<br>
@@ -162,14 +182,14 @@
                   </div>
                 </div>
               </section>
-             
+
               <section class="section-02">
-              <div class="ttl_wrap">
-                      <h2 class="ttl_01">IDEAL CANDIDATE</h2>
-                      <p class="lead">求める人物像</p>
-                    </div>
+                <div class="ttl_wrap">
+                  <h2 class="ttl_01">IDEAL CANDIDATE</h2>
+                  <p class="lead">求める人物像</p>
+                </div>
                 <div class="container">
-                <div class="img-box">
+                  <div class="img-box">
                     <img src="src/img/message-02.png" alt="">
                     <img src="src/img/message-03.png" alt="">
                     <img src="src/img/message-04.png" alt="">
@@ -191,37 +211,304 @@
                       <li>相手の立場に立てる人（思いやり）</li>
                     </ul>
                   </div>
-                  
+
                 </div>
               </section>
-             
+
+              <section class="section-03">
+                <div class="ttl_wrap">
+                  <h2 class="ttl_01">MEMBER</h2>
+                  <p class="lead">社員紹介</p>
+                </div>
+                <div class="container">
+
+                  <div class="contetnts">
+                    <?php
+                    $persons = [
+                      ['01', '営業', '0000'],
+                      ['02', '営業', '0000'],
+                      ['03', '営業', '0000'],
+                      ['04', '営業', '0000'],
+                    ]
+                    ?>
+                    <?php foreach ($persons as $value) : ?>
+                      <div class="pnl">
+                        <p class="person">
+                          <span>PERSON</span>
+                          <span><?php echo $value[0]; ?></span>
+                        </p>
+                        <div class="img">
+                          <img src="src/img/person-<?php echo $value[0]; ?>.jpg" alt="">
+                          <span class="position"><?php echo $value[1]; ?></span>
+                        </div>
+                        <p class="join"><?php echo $value[2]; ?>年入社</p>
+                      </div>
+                    <?php endforeach; ?>
+                  </div>
+
+                </div>
+              </section>
+              <section class="section-04">
+                <div class="ttl_wrap">
+                  <h2 class="ttl_01">WORK LIFE BALANCE</h2>
+                  <p class="lead">ワークライフバランス</p>
+                </div>
+                <div class="container">
+
+                  <div class="contetnts">
+                    <dl>
+                      <dd><span>年次有給休暇<br>取得率</span><span class="move"><span>67.6%</span></span><img src="src/img/icon-01.svg" alt=""></dd>
+                      <dd><img src="src/img/icon-02.svg" alt=""><span>年間休日</span><span class="move"><span>120</span></span><span class="strong">日以上</span></dd>
+                    </dl>
+                    <dl>
+                      <dd><span>中途入社実績</span><span class="move"><span>8</span></span><span class="strong">割超</span><img src="src/img/icon-03.svg" alt=""></dd>
+                      <dd><span>男女比率</span><span class="move"><span>2:1</span></span><img src="src/img/icon-04.svg" alt=""></dd>
+                      <dd><img src="src/img/icon-05.svg" alt=""><span>完全週休2日制</span><span class="move"><span>(土日) ・祝日</span></span></dd>
+                    </dl>
+                    <dl>
+                      <dd><img src="src/img/icon-06.svg" alt=""><span>定年後</span><span class="move"><span>再雇用<br>制度有</span></span></dd>
+                      <dd><img src="src/img/icon-07.svg" alt=""><span>業界</span><span class="move"><span>未経験可</span></span></dd>
+                      <dd><img src="src/img/icon-08.svg" alt=""><span>学歴・ 職歴</span><span class="move"><span>不問</span></span></dd>
+                      <dd><img src="src/img/icon-09.svg" alt=""><span class="move"><span>転勤</span></span><span>なし</span></dd>
+                    </dl>
+                    <dl>
+                      <dd><span>平均勤続年数</span><span class="move"><span>11.4</span></span><span class="strong">年</span><img src="src/img/icon-10.svg" alt=""></dd>
+                      <dd><span>平均年齢</span><span class="move"><span>38.9</span></span><span>歳</span><img src="src/img/icon-11.svg" alt=""></dd>
+                      <dd><span class="move"><span>第２新卒</span></span><span>採用実績あり</span><img src="src/img/icon-12.svg" alt=""></dd>
+                    </dl>
+                    <img class="icon" src="src/img/icon-13.png" alt="">
+                  </div>
+                </div>
+              </section>
+              <section class="section-05">
+                <div class="ttl_wrap">
+                  <h2 class="ttl_01">ALLOWANCES</h2>
+                  <p class="lead">諸手当</p>
+                </div>
+                <div class="container">
+                  <div class="contetnts">
+                    <img class="icon" src="src/img/icon-14.png" alt="">
+                    <dl>
+                      <?php
+                      $allowances = [
+                        ['01', '昇給あり', '年1回（8月）'],
+                        ['02', '賞与あり', '年2回（7月・12月）'],
+                        ['03', '交通費全額支給', '年2回（7月・12月）'],
+                        ['04', '出張手当', 'あり'],
+                        ['05', '食事手当', '月7,000円'],
+                        ['06', '家族手当', '配偶者：月5,000円<br>子：1人につき月3,000円'],
+                        ['07', '皆勤手当', '月1,500円'],
+                        ['08', '住宅手当', '月20,000円 ※本人名義'],
+                        ['09', '役職手当', '役職による'],
+
+                      ];
+                      ?>
+                      <?php foreach ($allowances as $value) : ?>
+                        <dd>
+                          <span><?php echo $value[0]; ?></span>
+                          <span><?php echo $value[1]; ?></span>
+                          <span><?php echo $value[2]; ?></span>
+                        </dd>
+                      <?php endforeach; ?>
+                    </dl>
+                  </div>
+                </div>
+              </section>
+              <section class="section-06">
+                <div class="ttl_wrap">
+                  <h2 class="ttl_01">BENEFITS</h2>
+                  <p class="lead">福利厚生</p>
+                </div>
+                <div class="container">
+                  <div class="contetnts">
+                    <p class="text">社内コミュニケーションの活性化は、組織の強化を促すだけでなく、新たなビジネスの創出にも繋がると考えています。弊社では、普段の業務で交流がなくても、お互いを知り、信頼関係を築き、結束を高められる機会があります。</p>
+                  </div>
+                  <div class="img-box">
+                    <img src="src/img/benefits-01.jpg" alt="">
+                  </div>
+                </div>
+                <div class="container">
+                  <dl>
+                    <dd><img src="src/img/btn.png" alt="">社会保険完備</dd>
+                    <dd><img src="src/img/btn.png" alt="">社員旅行</dd>
+                    <dd><img src="src/img/btn.png" alt="">退職金制度</dd>
+                    <dd><img src="src/img/btn.png" alt="">健康診断</dd>
+                    <dd><img src="src/img/btn.png" alt="">社員交流会<span>（食事会など/自由参加）</span></dd>
+                    <dd><img src="src/img/btn.png" alt="">分煙</dd>
+                    <dd><img src="src/img/btn.png" alt="">コーヒーサーバー<br>設置有</dd>
+                    <dd><img src="src/img/btn.png" alt="">服装自由<span>オフィスカジュアル、<br>髪型自由、ネイルOK</span></dd>
+                  </dl>
+                </div>
+              </section>
             </article>
+          </section>
+
+          <section class="section-07">
+            <div class="ttl_wrap">
+              <h2 class="ttl_01">COMPANY SCENERY</h2>
+              <p class="lead">社内風景</p>
+            </div>
+            <img class="icon" src="src/img/bg_section7.jpg" alt="">
+          </section>
+
+          <section class="section-08">
+            <div class="ttl_wrap">
+              <h2 class="ttl_01">REQUIREMENTS</h2>
+              <p class="lead">募集要項</p>
+            </div>
+            <div class="container">
+            <img class="icon" src="src/img/req-01.png" alt="">
+              <img class="icon" src="src/img/req-02.png" alt="">
+              <dl>
+                <dt>仕事内容</dt>
+                <dd>
+                  <h4>【既存顧客中心のBtoB営業】</h4>
+                  <p class="text">作業服や白衣、スポーツウェアなどの繊維製品（生地・服）の営業を行っていただきます。</p>
+                  <h4>【具体的には】</h4>
+                  <p class="text">日本全国のお客様に向けてご希望に応じた生地や製品をご提案します。<br>
+                    遠方の場合、出張（1週間以内）もありますが、自分でスケジュールを立てて行動することができます。</p>
+                  <p class="text">
+                    当社の営業は生産部門との距離が近いので、お客様の「こんなサンプルが欲しい」といった要望にも柔軟に対応出来ます！
+                  </p>
+                  <p class="text">POINT1 成長できる環境をご用意</p>
+                  <p class="text">
+                    顧客先はユニフォームアパレルメーカー（企業向けの制服・作業服）やスポーツ用品メーカー、繊維商社、縫製工場など様々。ひとつの業界との営業しかできないスタイルとは異なるので、様々な業界の知識が広がります。
+                  </p>
+                  <p class="text">POINT2 働きやすい環境</p>
+                  <p class="text">
+                    当社は若手社員が多く、いつも仲の良い社風です。<br>
+                    また、福利厚生やお休みも充実していることで社員満足度は高く、定着率も抜群！
+                  </p>
+                </dd>
+              </dl>
+              <dl>
+                <dt>求める人材</dt>
+                <dd>
+                  <p class="text">未経験者歓迎・第二新卒歓迎<br>
+                    ◆要普通自動車免許<br>
+                    ※人物重視の採用です。<br>
+                    異業種からの転職者も多数活躍中！</p>
+                  <h4>【必須条件】</h4>
+                  <p class="text">◆30歳以下の方（若年層の長期キャリア形成を図るため）<br>
+                    未経験者歓迎！まずはあなたの意欲を重視！</p>
+                  <p class="text">
+                    ★こんな方にピッタリ<br>
+                    主体的に動きたい!という方<br>
+                    社内外の人とのコミュニケーションを大切に出来る<br>
+                    一生役に立つスキルを身に付けたい<br>
+                    人を大切にする会社で、長く働きたい<br>
+                    自分の意志、考えを尊重してくれる会社で働きたい
+                  </p>
+                </dd>
+              </dl>
+              <dl>
+                <dt>勤務地</dt>
+                <dd>
+                  <p class="text">本社/大阪府大阪市中央区北久宝寺町1－2－15<br>※転勤なし</p>
+                  <p class="text">東京営業所/東京都中央区日本橋小伝馬町16－9豊鈴ビル503<br>※転勤なし</p>
+                  <h4>【アクセス(大阪)】</h4>
+                  <p class="text">
+                    地下鉄堺筋線「堺筋本町駅」より徒歩5分<br>
+                    地下鉄谷町線「谷町四丁目駅より」徒歩10分<br>
+                    地下鉄長堀鶴見緑地線「松屋町駅」より徒歩9分</p>
+                  <h4>【アクセス(東京)】</h4>
+                  <p class="text">
+                    地下鉄日比谷線「小伝馬町駅」より徒歩3分<br>
+                    地下鉄新宿線「馬喰横山駅より」徒歩3分<br>
+                    JR総武快速線「馬喰町駅」より徒歩3分</p>
+
+                </dd>
+              </dl>
+              <dl>
+                <dt>給与</dt>
+                <dd>
+                  <p class="text">月給20万～28万円＋諸手当<br>
+                    ※年齢・能力などを考慮し、決定します。</p>
+                  <p class="text">モデル年収例<br>
+                    年収320万円/27歳営業職経験3年<br>
+                    年収420万円/35歳営業職経験7年</p>
+                </dd>
+              </dl>
+              <dl>
+                <dt>昇給・賞与</dt>
+                <dd>
+                  <p class="text">◆昇給 : 年1回(8月)<br>◆賞与 : 年2回(7月・12月)</p>
+                </dd>
+              </dl>
+              <dl>
+                <dt>諸手当</dt>
+                <dd>
+                  <p class="text">交通費(全額)<br>
+                    食事手当て(7000円/月)<br>
+                    出張手当<br>
+                    住宅手当(2万円/月　独身で実家が近畿圏外の方)<br>
+                    家族手当(配偶者 : 5000円/月、子：一人につき月3000円/月)<br>
+                    役職手当(1500円/月)<br>
+                    精勤手当(3万円/年)
+                  </p>
+                </dd>
+              </dl>
+              <dl>
+                <dt>休日・休暇</dt>
+                <dd>
+                  <p class="text">完全週休2日制(土・日)<br>
+                    祝日<br>
+                    年末年始休暇<br>
+                    夏季休暇<br>
+                    GW<br>
+                    有給休暇<br>
+                    慶弔休暇<br>
+                    年間休日120日
+                  </p>
+                </dd>
+              </dl>
+              <dl>
+                <dt>福利厚生</dt>
+                <dd>
+                  <p class="text">社会保険完備(雇用・労災・健康・厚生年金)<br>
+                    社員旅行<br>
+                    退職金制度
+                  </p>
+                </dd>
+              </dl>
+              <dl>
+                <dt>面接回数</dt>
+                <dd>
+                  <p class="text">1～2回
+                  </p>
+                </dd>
+              </dl>
+              <dl>
+                <dt>面接地</dt>
+                <dd>
+                  <h4>【本社】</h4>
+                  <p class="text">〒541－0057<br>
+                    大阪府大阪市中央区北久宝寺1-2-15</p>
+                  <h4>【東京営業所】</h4>
+                  <p class="text">〒103-0001<br>
+                    東京都中央区日本橋小伝馬町16－9豊鈴ビル503</p>
+                </dd>
+              </dl>
+              <dl>
+                <dt>選考の特徴</dt>
+                <dd>
+                  <p class="text">夜間面接OK<br>
+                    土日面接OK<br>
+                    面接日程応相談<br>
+                    入社時期相談<br>
+                    応募から内定まで1カ月</p>
+                </dd>
+              </dl>
+             
+            </div>
+            <div class="btn_area">
+              <a href="entry" class="btn">ENTRY</a>
+            </div>
           </section>
         </div>
       </div>
 
-      <section id="cv_area">
-        <div class="sec_inner">
-          <div class="ttl_wrap">
-            <p class="en">CONTACT</p>
-            <h2 class="ttl_01">お問い合わせ</h2>
-          </div>
-          <p>製品やサービスに関するご相談・ご質問、<br class="hidden_sp">テキスタイルのご注文などございましたら、<br class="hidden_sp">お気軽にお問い合わせください。</p>
-          <div class="contact_area">
-            <div class="l_box">
-              <p class="m_txt">お電話でのお問い合わせ</p>
-              <p class="tel"><a href="tel:0662641161">06-6264-1161</a></p>
-              <p class="s_txt">受付時間 9:30-18:00 ( 土・日・祝日除く)</p>
-            </div>
-            <div class="r_box">
-              <a href="https://www.nonoguchi.net/contact/">お問い合わせ</a>
-              <a href="https://www.nonoguchi.net/order/">注文フォーム</a>
-              <a href="https://www.nonoguchi.net/stock/">在庫確認</a>
-              <a href="https://www.nonoguchi.net/cloth/">生地サンプル依頼</a>
-            </div>
-          </div>
-        </div>
-      </section>
+
 
       <footer id="footer">
         <div class="top_box">
